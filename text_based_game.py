f33d5c0e098d886f303e4b92dc8966d650f005b9
crestel-ong <crestel.ong@mths.ca>
2021-11-08 17:33:33 +0000

    lesson 1

---
+++ text_based_game.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Crestel Ong
+# Created on: Nov 2021
+# this is a text based game for learning guide 3
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
